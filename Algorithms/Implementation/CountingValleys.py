d9150e52042bd722d60803a8cf65ea89034928bc
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-09-12 10:57:18 +0530

    Create CountingValleys.py

---
+++ Algorithms/Implementation/CountingValleys.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    valleys = 0
+    currentlevel = 0
+    for letter in s:
+        if letter=="U":
+            currentlevel += 1
+        else:
+            currentlevel -= 1
+        if currentlevel==0 and letter=="U":
+            valleys += 1
+    return valleys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
