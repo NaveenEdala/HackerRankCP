72a4b9aefdf50505abe7f7ef4d1fb4643a3c4eb4
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-09-12 10:48:00 +0530

    Create SimpleArraySum.py

---
+++ Algorithms/Warmup/SimpleArraySum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    
+    arsum = 0 
+    for i in ar:
+        arsum += i
+    return arsum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
