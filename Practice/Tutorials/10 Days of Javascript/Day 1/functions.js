d5e5580de51c646465ffcd9edd42ff58c64ada2b
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-10-03 05:32:55 +0530

    Create functions.js

---
+++ Practice/Tutorials/10 Days of Javascript/Day 1/functions.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    var facresult = n;
+    n--;
+    while (n > 0) {
+        facresult = facresult * n;
+        n--;
+    }
+    return facresult;
+}
+
+
