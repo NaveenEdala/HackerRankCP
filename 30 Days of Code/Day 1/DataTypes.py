0692220f63813bae3e9b135fce61fbd88460d6ee
Naveen Edala <44523972+NaveenEdala@users.noreply.github.com>
2019-12-14 11:13:03 +0530

    Create DataTypes.py

---
+++ 30 Days of Code/Day 1/DataTypes.py
@@ -0,0 +1,14 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = int(input())
+b = float(input())
+c = input()
+# Print the sum of both integer variables on a new line.
+print(i+a)
+# Print the sum of the double variables on a new line.
+print("%.1f" % (d+b))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+c)
